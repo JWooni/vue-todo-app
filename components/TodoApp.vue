77d0d83398d240325d1b80cef20c3ae35b32dc65
JWooni <wjddns59@naver.com>
2021-06-14 20:45:21 +0900

    commit

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -18,6 +18,15 @@
           완료된 항목 ({{completedCount}})
         </button>
       </div>
+
+      <div class="actions">
+        <input
+         v-model="allDone"
+         type="checkbox">
+        <button>
+          완료된 항목 삭제
+        </button>
+      </div>
     </div>
 
     <div class="todo-app__list">
@@ -81,6 +90,14 @@ export default {
     },
     completedCount () {
       return this.total - this.activeCount
+    },
+    allDone: {
+      get () {
+        return this.total === this.completedCount && this.total > 0
+      },
+      set (checked) {
+        this.completeAll(checked)
+      }
     }
   },
   created () {
@@ -145,6 +162,21 @@ export default {
     },
     changeFilter (filter) {
       this.filter = filter
+    },
+    completeAll (checked) {
+      // DB
+      const newTodos = this.db
+      .get('todos')
+      .forEach(todo => {
+        todo.done = checked
+      })
+      .write()
+
+      // Local todos
+      // this.todos.forEach(todo => {
+      //   todo.done = checked
+      // })
+      this.todos = _cloneDeep(newTodos)
     }
   }
 }
