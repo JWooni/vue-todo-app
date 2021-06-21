416f052ee59994062d0d30224ce148efbf92f87a
JWooni <wjddns59@naver.com>
2021-06-21 20:05:08 +0900

    commit

--- store/index.js
+++ store/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import todoApp from './todoApp'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production'
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    todoApp
+  }
 })
