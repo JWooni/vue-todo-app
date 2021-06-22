4abc45fa23be5d1ed000ddba88673d1dca65fb00
JWooni <wjddns59@naver.com>
2021-06-22 21:16:05 +0900

    commit

--- store/todoApp.js
+++ store/todoApp.js
@@ -4,19 +4,39 @@ export default {
       db: null,
       todos: []    
   }),
-  getters: {},
-  mutations: {},
+  getters: {
+    total (state) {
+      return state.todos.length
+    },
+    activeCount (state) {
+      return state.todos.filter(todo => !todo.done).length
+    },
+    completedCount (state, getters) {
+      return getters.total - getters.activeCount
+    }
+  },
+  mutations: {
+    assignDB(state, db) {
+      state.db = db
+    },
+    assignTodos(state, todos) {
+      state.todos = todos
+    }
+  },
   actions: {
-    initDB({ state }) {
+    initDB({ state, commit }) {
       const adapter = new LocalStorage('todo-app') // DB name
-      state.db = low(adapter)
+      // state.db = low(adapter)
+      commit('assignDB', low(adapter))
+
       const hasTodos = state.db
         .has('todos') // Collection name
         .value()
       // 기존에 저장된 DB가 있는지 확인
       if (hasTodos) {
         // 깊은 배열 복사, `state.todos`를 수정할 때 `state.db.getState().todos`를 직접 참조하는 문제를 방지할 수 있습니다.
-        state.todos = _cloneDeep(state.db.getState().todos)
+        //state.todos = _cloneDeep(state.db.getState().todos)
+        commit('assignTodos', state.todos = _cloneDeep(state.db.getState().todos))
       } else {
         // Local DB 초기화
         state.db
